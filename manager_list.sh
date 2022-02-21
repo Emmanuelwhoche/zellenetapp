0de727814c74c054dd7039f8e6b426aa8dc7c1b2
emmanuel onyeabor <emmanuelucheonyeabor@gmail.com>
2022-02-21 01:07:42 -0500

    list to be reviewed

---
+++ manager_list.sh
@@ -0,0 +1,3 @@
+#list of engr to impliment a job
+This is to inform the team that our engrs will be accountable for the installation of ibm server and plant
+and the managers will be assigned there duties during this pratice.
